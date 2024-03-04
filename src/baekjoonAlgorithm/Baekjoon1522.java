8d322904f13408a7cb78915803a94277a8684572
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-04 17:14:52 +0900

    style: Remove comments

--- src/baekjoonAlgorithm/Baekjoon1522.java
+++ src/baekjoonAlgorithm/Baekjoon1522.java
@@ -31,29 +31,6 @@ public class Baekjoon1522 {
         }
 
         System.out.println(min);
-//        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-//        char[] str=br.readLine().toCharArray();
-//
-//        int aSize=0;
-//
-//        for(int i=0;i<str.length;i++){
-//            if(str[i]=='a') aSize++;
-//        }
-//
-//        int start=0,end=aSize-1,bCount=0;
-//        for(int i=0;i<aSize;i++){
-//            if(str[i]=='b') bCount++;
-//        }
-//
-//        int min=bCount;
-//        while(start<str.length){
-//            if(str[++end%str.length]=='b') bCount++;
-//            if(str[start++]=='a') bCount--;
-//
-//            min=Math.min(min,bCount);
-//        }
-//
-//        System.out.println(min);
 
     }
 }
