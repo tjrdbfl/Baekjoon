42e74439e453db88fe26a8bbff82ec355fd06114
tjrdbfl <tjrdbfl123@gmail.com>
2024-03-04 18:06:05 +0900

    feat: Arrays.sort using lambda expression

--- src/baekjoonAlgorithm/Baekjoon1931.java
+++ src/baekjoonAlgorithm/Baekjoon1931.java
@@ -15,14 +15,11 @@ public class Baekjoon1931 {
             times[i][1] = sc.nextInt();
         }
 
-        Arrays.sort(times, new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1]){
-                    return o1[0]-o2[0];
-                }
-                return o1[1]-o2[1];
-            }
-        });
+        Arrays.sort(times,((o1, o2) -> {
+            if(o1[1]==o2[1])
+                return o1[0]-o2[0];
+            else return o1[1]-o2[1];
+        }));
 
 
         for(int i = 0; i < times.length; i++) {
